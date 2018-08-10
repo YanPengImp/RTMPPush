e309869eecc68c778fad6bbcb7788d31fd87bb1b
imp <yp@jingbo360.com>
2018-08-10 09:35:07 +0800

    Initial Commit

---
+++ FilterCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FilterCamera
+//
+//  Created by Imp on 2018/8/10.
+//  Copyright © 2018年 jingbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FilterCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FilterCamera
+//
+//  Created by Imp on 2018/8/10.
+//  Copyright © 2018年 jingbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
